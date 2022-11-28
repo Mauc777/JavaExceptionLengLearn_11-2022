abd43be55750be0e3ba9c147b8f0d6b65a184348
M777 <Kozannostra777@mail.ru>
2022-11-28 14:08:32 +0300

    11

---
+++ Seminar1Homework/ArrayMas.java
@@ -0,0 +1,46 @@
+
+// Реализуйте метод, принимающий в качестве аргументов два целочисленных массива,
+// и возвращающий новый массив, каждый элемент которого равен разности 
+// элементов двух входящих массивов в той же ячейке. Если длины массивов не равны, 
+//необходимо как-то оповестить пользователя.
+//public class ArrayMas {необходимо как-то оповестить пользователя.
+
+
+public class ArrayMas {
+
+    //int[] a = { 1, 2, 3, 4, 5, 6, 8, 9, 2, 4 };
+    //int[] b = { 1, 2, 3, 10, 5, 5, 8, 8, 2, 4 };
+     public static void main(String[] args) {
+        int[] a = { 1, 2, 3, 4, 5, 6, 8, 9, 2, 4 };
+        int[] b = { 1, 2, 3, 3, 4, 6, 8, 8, 2 ,4};
+        massivCreateNewC(a, b);
+        
+    }
+
+    public static void massivCreateNewC(int[] a, int[] b) {
+        //int[] c = {};
+        if (a.length == b.length) {
+            //объявление массива arr3 по размерности одного из массивов
+            int[] arr3 = new int[a.length];
+            int idexForArr3 = 0;
+            for (int i = 0; i < arr3.length; i++) {
+                arr3[idexForArr3] = a[i] - b[i];
+                idexForArr3 += 1;
+
+            }
+            System.out.println();
+            System.out.println("Ваш новый массив: " + arr3);
+            //System.out.println("Второй вариант отображения");
+            //for (int i = 0; i < arr3.length; i++) {
+            //    System.out.println(arr3[i] + "\t");
+        }
+            
+        else {
+            System.out.println("Длины массивов не равно, а значит это что- то значит");
+        }
+        
+    }
+    
+   
+    
+}
--- Seminar1Homework/ExceptionMethod.java
+++ Seminar1Homework/ExceptionMethod.java
@@ -1,30 +1,31 @@
 import java.io.File;
 import java.util.Collections;
 import java.util.List;
+//Задание к Семинару_1 JAvaException
+//Реализуйте 3 метода,чтобы в каждом из них получить разные исключения Посмотрите на код,
+//и подумайте сколько разных типов исключений вы тут сможете получить?
 
 public class ExceptionMethod {
     public static void main(String[] args) {
-
         // 1 ArithmeticException
         // Method1(10, 0);
 
-        // 2UnsupportedOperationException
+        // 2 UnsupportedOperationException
         // Method2(2);
         // Method2("sdvsv");
 
         // #3 ClassCastException:
         // Method3(123);
-
     }
 
-    // #1
+    // #1 ArithmeticException
     public static void Method1(int a, int b) {
         int result = 0;
         result = result + (a / b);
         System.out.println(result);
     }
 
-    // #2
+    // #2 UnsupportedOperationException
     public static void Method2(Object o) {
         List<Object> emptyList = Collections.emptyList();
         emptyList.add(new Object());
