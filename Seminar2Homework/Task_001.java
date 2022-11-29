c805a57ba297bcb6995f66f1666e390760ec5510
M777 <Kozannostra777@mail.ru>
2022-11-29 22:04:03 +0300

    Набросок для работы

--- Seminar2Homework/Task_001.java
+++ Seminar2Homework/Task_001.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 //*Задание 1 реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float), 
 //*и возвращает введенное значение. Ввод текста вместо числа не должно приводить к падению приложения, 
@@ -5,7 +6,15 @@
 //*import java.io.IOException;
 
 public class Task_001 {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите дробное число через ' , ' : ");
+        if (scan.hasNextFloat()) {
+            float i = scan.nextFloat();
+            System.out.println(i);
+        } else {
+            System.out.println("Вы ввели не дробное число");
+        }
      
  
     }
